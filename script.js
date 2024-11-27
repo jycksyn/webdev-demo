3ce02eba24527965017f60d722b5f36f11cd337d
jackson <jackson.kubin@gmail.com>
2024-11-26 23:31:23 -0500

    solve

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+document.getElementById("submitButton").addEventListener("click", addTask);
+
+
+function addTask(e){
+    var li = document.createElement('li');
+    li.innerText = document.querySelector("#taskInput").value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className="button";
+    deleteButton.innerText="x";
+    deleteButton.addEventListener("click", () => taskList.removeChild(li));
+    li.append(deleteButton)
+    document.querySelector("#taskList").append(li);
+}
